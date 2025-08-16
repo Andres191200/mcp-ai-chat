d32ca2a98ee5d5183a3a2f12b984c424f5e02010
Andres <marianoandres191200@hotmail.com>
2025-08-16 16:16:47 -0300

    save

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,46 +1,25 @@
 import styles from "./styles.module.scss";
 import MessageInputBar from "../message-input-bar/MessageInputBar";
 import Messages from "../messages/Messages";
-import { sendMessageToDb } from "../../db/db";
 import { useState } from "react";
-import DOMPurify from "dompurify";
+import sendMessage from "../../actions/sendMessage";
 
 export default function Chat() {
   const [loading, setLoading] = useState<boolean>(false);
 
-  async function sendMessage(message: string) {
-    const sanitizedMessage = DOMPurify.sanitize(message);
-    const userID = 3;
+  async function onSubmit(message: string) {
     setLoading(true);
-    sendMessageToDb({
-      message: sanitizedMessage,
-      userID,
-      date: Date.now(),
-      userName: "Andrés",
-    });
-    if (message.includes("/prompt")) {
-      try {
-        await fetch("http://localhost:3000/prompt", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: sanitizedMessage }),
-        });
-      } catch (error) {
-        console.log("error: ", error);
-      } finally {
-        setLoading(false);
-      }
-    } else {
-      setLoading(false);
-    }
+    const res = await sendMessage(message);
+    console.log('OLLAMA RESPONSE: ', res);
+    console.log('OLLAMA RESPONSE BODY: ', res?.body);
+
+    setLoading(false);
   }
 
   return (
     <div className={styles.chatComponent}>
       <Messages />
-      <MessageInputBar onSendMessage={sendMessage} disabled={loading} />
+      <MessageInputBar onSubmit={onSubmit} disabled={loading} />
     </div>
   );
 }
