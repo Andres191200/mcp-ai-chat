6051d083fd7b33a1525c076b1544c8e3312b682a
Andres <marianoandres191200@hotmail.com>
2025-08-17 14:40:59 -0300

    displaying the AI response on the chat as a bubble message

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -3,6 +3,7 @@ import MessageInputBar from "../message-input-bar/MessageInputBar";
 import Messages from "../messages/Messages";
 import { useState } from "react";
 import sendMessage from "../../actions/sendMessage";
+import { sendMessageToDb } from "../../db/db";
 
 export default function Chat() {
   const [loading, setLoading] = useState<boolean>(false);
@@ -10,9 +11,15 @@ export default function Chat() {
   async function onSubmit(message: string) {
     setLoading(true);
     const res = await sendMessage(message);
-    const parsedRes = await res?.json();
-    console.log('OLLAMA PARSED RES: ', parsedRes);
-
+    const {answer} = await res!.json();
+    console.log('parsedRes_2: ', JSON.parse(answer));
+    sendMessageToDb({
+      message: JSON.parse(answer).answer,
+      userName: "AI",
+      date: Date.now(),
+      userID: 0,
+    });
+    
     setLoading(false);
   }
 
