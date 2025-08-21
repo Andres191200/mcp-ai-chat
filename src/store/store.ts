31c4fd56f880f37ce0a60618b1aee5a57b59e1ae
Andres <marianoandres191200@hotmail.com>
2025-08-20 22:21:59 -0300

    adding delete function

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,22 +4,30 @@ import { sendMessageToDb, type TMessage } from "../db/db";
 type TMessagesStore = {
   messages: TMessage[];
   sendMessage: (message: TMessage) => void;
+  addClientMessage: (message: TMessage) => void;
+  updateClientMessage: (message: TMessage) => void;
+  deleteClientMessage: (message: TMessage) => void;
 };
 
-export const useMessagesStore = create<TMessagesStore>((set) => ({
+export const useMessagesStore = create<TMessagesStore>((set):TMessagesStore => ({
   messages: [],
+
+  // DATABASE MESSAGE SENDER FUNCTION
   sendMessage: (message: TMessage) => {
     set((state: TMessagesStore) => {
       return { messages: [...state.messages, message] };
     });
     sendMessageToDb({ ...message });
   },
+
   // CLIENT MESSAGE BECAUSE IT ONLY HAPPENS ON THE CLIENT SIDE, WITHOUT SENDING TO DB
   addClientMessage: (message: TMessage) => {
     set((state: TMessagesStore) => ({
       messages: [...state.messages, message],
     }));
   },
+
+  // UPDATING MESSAGE ONLY ON THE CLIENT SIDE, NOT DB
   updateClientMessage: (message: TMessage) =>
     set((state: TMessagesStore) => ({
         messages: state.messages.map((msg) => {
@@ -28,5 +36,10 @@ export const useMessagesStore = create<TMessagesStore>((set) => ({
             }
             return msg;
         })
-    }))
+    })),
+    deleteClientMessage: (message: TMessage) =>
+    set((state: TMessagesStore) => ({
+        messages: state.messages.filter((msg) => msg.date !== message.date)
+    })),
+
 }));
