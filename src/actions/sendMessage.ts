9d7b2d6beddef167a7e633886de7ab0f7f84578a
Andres <marianoandres191200@hotmail.com>
2025-08-23 13:47:33 -0300

    code small documentation and removing "/prompt" filtering to call the LLM

--- src/actions/sendMessage.ts
+++ src/actions/sendMessage.ts
@@ -4,37 +4,45 @@ import { useMessagesStore } from "../store/store";
 export default async function sendMessage(message: string) {
   const sanitizedMessage = DOMPurify.sanitize(message);
   const userID = 3;
+
+  // 1 - SENDING USER MESSAGE TO DB
   useMessagesStore.getState().sendMessage({
     message: sanitizedMessage,
     userID,
     date: Date.now(),
     userName: "Andrés",
   });
-  if (message.includes("/prompt")) {
-    const tempID = Date.now();
+
+  // 2 - CREATING TEMP MESSAGE FOR OPTIMISTIC UI PURPOSES
+  const tempID = Date.now();
+  useMessagesStore.getState().sendMessage({
+    message: "Thinking...",
+    userName: "AI",
+    date: tempID,
+    userID: 0,
+  });
+
+  // 3 - FETCHING THE LLM THROUGH BACKEND
+  try {
+    const response = await fetch("http://localhost:3000/prompt", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: sanitizedMessage }),
+    });
+
+    // 4 - GETTING LLM RESPONSE FROM BACKEND
+    const { answer } = await response.json();
+
+    // 5 - SENDING MESSAGE TO DB FOR CENTRALIZING PURPOSES
     useMessagesStore.getState().sendMessage({
-      message: "Thinking...",
+      message: JSON.parse(answer).answer,
       userName: "AI",
       date: tempID,
       userID: 0,
     });
-    try {
-      const response = await fetch("http://localhost:3000/prompt", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt: sanitizedMessage }),
-      });
-      const {answer} = await response.json();
-      useMessagesStore.getState().sendMessage({
-        message: JSON.parse(answer).answer,
-        userName: "AI",
-        date: tempID,
-        userID: 0,
-      });
-    } catch (error) {
-      console.log("error: ", error);
-    }
+  } catch (error) {
+    console.log("error: ", error);
   }
 }
