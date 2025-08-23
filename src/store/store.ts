be509e149d63286ddd5077a3199a9164e5dbd266
Andres <marianoandres191200@hotmail.com>
2025-08-23 14:49:45 -0300

    moving all db logic to a own backend route. Removing app and db interaction

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,45 +1,64 @@
 import { create } from "zustand";
-import { sendMessageToDb, type TMessage } from "../db/db";
+import { type TMessage } from "../db/db";
+import sendMessage from "../actions/sendMessage";
+import DOMPurify from "dompurify";
 
 type TMessagesStore = {
   messages: TMessage[];
-  sendMessage: (message: TMessage) => void;
+  sendMessage: (message: string) => void;
   addClientMessage: (message: TMessage) => void;
   updateClientMessage: (message: TMessage) => void;
   deleteClientMessage: (message: TMessage) => void;
 };
 
-export const useMessagesStore = create<TMessagesStore>((set):TMessagesStore => ({
-  messages: [],
+export const useMessagesStore = create<TMessagesStore>(
+  (set): TMessagesStore => ({
+    messages: [],
 
-  // DATABASE MESSAGE SENDER FUNCTION
-  sendMessage: (message: TMessage) => {
-    set((state: TMessagesStore) => {
-      return { messages: [...state.messages, message] };
-    });
-    sendMessageToDb({ ...message });
-  },
+    // DATABASE MESSAGE SENDER FUNCTION
+    sendMessage: (message: string) => {
+      const sanitizedMessage = DOMPurify.sanitize(message);
+      const userID = 3;
+      const messageToDb:TMessage = {
+              message: sanitizedMessage,
+              userID,
+              date: Date.now(),
+              userName: "Andrés",
+            };
+      set((state: TMessagesStore) => {
+        return {
+          messages: [
+            ...state.messages,
+            messageToDb,
+          ],
+        };
+      });
+      // sendMessageToDb({ ...message });
+      sendMessage(messageToDb);
 
-  // CLIENT MESSAGE BECAUSE IT ONLY HAPPENS ON THE CLIENT SIDE, WITHOUT SENDING TO DB
-  addClientMessage: (message: TMessage) => {
-    set((state: TMessagesStore) => ({
-      messages: [...state.messages, message],
-    }));
-  },
+      //CHECK IF MESSAGE CONTAINS "/PROMPT" HERE?
+    },
 
-  // UPDATING MESSAGE ONLY ON THE CLIENT SIDE, NOT DB
-  updateClientMessage: (message: TMessage) =>
-    set((state: TMessagesStore) => ({
+    // CLIENT MESSAGE BECAUSE IT ONLY HAPPENS ON THE CLIENT SIDE, WITHOUT SENDING TO DB (ONLY FOR OPTIMISTIC PURPOSES)
+    addClientMessage: (message: TMessage) => {
+      set((state: TMessagesStore) => ({
+        messages: [...state.messages, message],
+      }));
+    },
+
+    // UPDATING MESSAGE ONLY ON THE CLIENT SIDE, NOT DB (ONLY FOR OPTIMISTIC PURPOSES)
+    updateClientMessage: (message: TMessage) =>
+      set((state: TMessagesStore) => ({
         messages: state.messages.map((msg) => {
-            if (msg.date === message.date) {
-                return message;
-            }
-            return msg;
-        })
-    })),
+          if (msg.date === message.date) {
+            return message;
+          }
+          return msg;
+        }),
+      })),
     deleteClientMessage: (message: TMessage) =>
-    set((state: TMessagesStore) => ({
-        messages: state.messages.filter((msg) => msg.date !== message.date)
-    })),
-
-}));
+      set((state: TMessagesStore) => ({
+        messages: state.messages.filter((msg) => msg.date !== message.date),
+      })),
+  })
+);
