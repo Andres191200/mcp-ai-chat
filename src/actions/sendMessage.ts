871522da86d2bc58efcc430fc4459e47352e5557
Andres <marianoandres191200@hotmail.com>
2025-08-23 13:56:37 -0300

    changed fetching functions order

--- src/actions/sendMessage.ts
+++ src/actions/sendMessage.ts
@@ -4,8 +4,9 @@ import { useMessagesStore } from "../store/store";
 export default async function sendMessage(message: string) {
   const sanitizedMessage = DOMPurify.sanitize(message);
   const userID = 3;
+  const tempID = Date.now();
 
-  // 1 - SENDING USER MESSAGE TO DB
+  // 1 - SENDING THE USER MESSAGE TO DB
   useMessagesStore.getState().sendMessage({
     message: sanitizedMessage,
     userID,
@@ -13,35 +14,35 @@ export default async function sendMessage(message: string) {
     userName: "Andrés",
   });
 
-  // 2 - CREATING TEMP MESSAGE FOR OPTIMISTIC UI PURPOSES
-  const tempID = Date.now();
-  useMessagesStore.getState().sendMessage({
-    message: "Thinking...",
-    userName: "AI",
-    date: tempID,
-    userID: 0,
-  });
-  // 3 - FETCHING THE LLM THROUGH BACKEND
-  try {
-    const response = await fetch("http://localhost:3000/prompt", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ prompt: sanitizedMessage }),
-    });
+  // 2 - ASKING LLM TO SAVE ANY PARTICULAR "TOOL" IF MATCHES
+      try {
+      const response = await fetch("http://localhost:3000/prompt", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: sanitizedMessage }),
+      });
+      const {answer} = await response.json();
 
-    // 4 - GETTING LLM RESPONSE FROM BACKEND
-    const { answer } = await response.json();
-
-    // 5 - SENDING MESSAGE TO DB FOR CENTRALIZING PURPOSES
+      // 4 - WHEN THE LLM RESPONSE IS READY, SEND IT AS A MESSAGE TO THE DB AND THE UI
+      useMessagesStore.getState().sendMessage({
+        message: JSON.parse(answer).answer,
+        userName: "AI",
+        date: tempID,
+        userID: 0,
+      });
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  
+  // 3 - IF THE USER MESSAGE CONTAINS "/PROMPT", THEN ADD A OPTIMISTIC MESSAGE ON THE CHAT
+  if (message.includes("/prompt")) {
     useMessagesStore.getState().sendMessage({
-      message: JSON.parse(answer).answer,
+      message: "Thinking...",
       userName: "AI",
       date: tempID,
       userID: 0,
     });
-  } catch (error) {
-    console.log("error: ", error);
   }
 }
