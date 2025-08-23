13a346d6f7c9a4aecc03e2d88cc8bb52f3e2213c
Andres <marianoandres191200@hotmail.com>
2025-08-23 14:05:10 -0300

    revert

--- src/actions/sendMessage.ts
+++ src/actions/sendMessage.ts
@@ -4,18 +4,21 @@ import { useMessagesStore } from "../store/store";
 export default async function sendMessage(message: string) {
   const sanitizedMessage = DOMPurify.sanitize(message);
   const userID = 3;
-  const tempID = Date.now();
-
-  // 1 - SENDING THE USER MESSAGE TO DB
   useMessagesStore.getState().sendMessage({
     message: sanitizedMessage,
     userID,
     date: Date.now(),
     userName: "Andrés",
   });
-
-  // 2 - ASKING LLM TO SAVE ANY PARTICULAR "TOOL" IF MATCHES
-      try {
+  if (message.includes("/prompt")) {
+    const tempID = Date.now();
+    useMessagesStore.getState().sendMessage({
+      message: "Thinking...",
+      userName: "AI",
+      date: tempID,
+      userID: 0,
+    });
+    try {
       const response = await fetch("http://localhost:3000/prompt", {
         method: "POST",
         headers: {
@@ -24,8 +27,6 @@ export default async function sendMessage(message: string) {
         body: JSON.stringify({ prompt: sanitizedMessage }),
       });
       const {answer} = await response.json();
-
-      // 4 - WHEN THE LLM RESPONSE IS READY, SEND IT AS A MESSAGE TO THE DB AND THE UI
       useMessagesStore.getState().sendMessage({
         message: JSON.parse(answer).answer,
         userName: "AI",
@@ -35,14 +36,5 @@ export default async function sendMessage(message: string) {
     } catch (error) {
       console.log("error: ", error);
     }
-  
-  // 3 - IF THE USER MESSAGE CONTAINS "/PROMPT", THEN ADD A OPTIMISTIC MESSAGE ON THE CHAT
-  if (message.includes("/prompt")) {
-    useMessagesStore.getState().sendMessage({
-      message: "Thinking...",
-      userName: "AI",
-      date: tempID,
-      userID: 0,
-    });
   }
 }
