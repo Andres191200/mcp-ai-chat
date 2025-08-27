2944fdfa8a62c79998af462b17347f1f05e29e65
Andres <marianoandres191200@hotmail.com>
2025-08-26 22:05:57 -0300

    fix syntax and removing commented code

--- src/actions/sendMessage.ts
+++ src/actions/sendMessage.ts
@@ -1,41 +1,3 @@
-// export default async function sendMessage(message: string) {
-//   const sanitizedMessage = DOMPurify.sanitize(message);
-//   const userID = 3;
-//   useMessagesStore.getState().sendMessage({
-//     message: sanitizedMessage,
-//     userID,
-//     date: Date.now(),
-//     userName: "Andrés",
-//   });
-//   if (message.includes("/prompt")) {
-//     const tempID = Date.now();
-//     useMessagesStore.getState().sendMessage({
-//       message: "Thinking...",
-//       userName: "AI",
-//       date: tempID,
-//       userID: 0,
-//     });
-//     try {
-//       const response = await fetch("http://localhost:3000/prompt", {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({ prompt: sanitizedMessage }),
-//       });
-//       const {answer} = await response.json();
-//       useMessagesStore.getState().sendMessage({
-//         message: JSON.parse(answer).answer,
-//         userName: "AI",
-//         date: tempID,
-//         userID: 0,
-//       });
-//     } catch (error) {
-//       console.log("error: ", error);
-//     }
-//   }
-// }
-
 import type { TMessage } from "../db/db";
 
 export default async function sendMessage(message: TMessage) {
@@ -46,24 +8,9 @@ export default async function sendMessage(message: TMessage) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(message),
+
     });
-    try {
-      const response = await fetch("http://localhost:3000/prompt", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt: sanitizedMessage }),
-      });
-      const {answer} = await response.json();
-      useMessagesStore.getState().sendMessage({
-        message: JSON.parse(answer).answer,
-        userName: "AI",
-        date: tempID,
-        userID: 0,
-      });
-    } catch (error) {
-      console.log("error: ", error);
-    }
+  } catch (error) {
+    console.log("error: ", error);
   }
 }
